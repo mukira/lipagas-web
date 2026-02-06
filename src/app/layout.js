e8268b46ae8005567e410744bd11049bd280d9a6
Mukira <rojdevensky@gmail.com>
2026-02-06 16:46:45 +0300

    🦎 Chameleon Mode Activated: Logo now adapts dynamically! + Favicon fix 🚀

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import './globals.css';
 export const metadata = {
   title: 'Lipagas',
   description: 'Lipagas Web Store',
+  icons: {
+    icon: '/favicon.png', // /public/favicon.png
+    apple: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
