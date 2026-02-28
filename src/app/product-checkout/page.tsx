1dd07b32c2d59202ad60b6e0aa35c6a15573254b
Mukira <rojdevensky@gmail.com>
2026-03-01 02:18:44 +0300

    🚀 Feature: Duplicated checkout page to create new /product-checkout route

---
+++ src/app/product-checkout/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+    return (
+        <iframe
+            src="/pages/product-checkout.html"
+            style={{
+                width: '100%',
+                height: '100vh',
+                border: 'none',
+                display: 'block'
+            }}
+            title="Product Checkout"
+        />
+    );
+}
