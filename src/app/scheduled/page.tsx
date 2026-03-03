ffb9b7b2ed5d3e1ed032eb6ae49ea6f49339a000
Mukira <rojdevensky@gmail.com>
2026-03-03 18:35:51 +0300

    🚀 We Move Gas, You Move Markets. Scheduled Deliveries now live and fueled up! 🚛💨

---
+++ src/app/scheduled/page.tsx
@@ -0,0 +1,15 @@
+
+export default function ScheduledPage() {
+    return (
+        <iframe
+            src="/pages/scheduled.html"
+            style={{
+                width: '100vw',
+                height: '100vh',
+                border: 'none',
+                display: 'block',
+            }}
+            title="Scheduled"
+        />
+    );
+}
