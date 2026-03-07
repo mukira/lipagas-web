5c5c2f0f6444825d9909b2ef75985362b5a885f3
Mukira <rojdevensky@gmail.com>
2026-03-07 04:19:16 +0300

    🚀 The Era of Smart Energy: Reimagined 'About Us' with Steve Jobs & Uber ethos

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutUsPage() {
+  return (
+    <iframe
+      src="/pages/about-us.html"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+        display: 'block',
+      }}
+      title="about us"
+    />
+  );
+}
