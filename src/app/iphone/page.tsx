bf0588249c09aafcb89842ba56baf9f0fb169d5c
Mukira <rojdevensky@gmail.com>
2026-03-15 18:52:38 +0300

    chore: consolidate to iphone.html and clean up routing

--- src/app/iphone/page.tsx
+++ src/app/iphone/page.tsx
@@ -1,7 +1,7 @@
 export default function IphonePage() {
   return (
     <iframe
-      src="/pages/index.html"
+      src="/pages/iphone.html"
       style={{
         width: '100vw',
         height: '100vh',
